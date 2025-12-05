816a4d32765f9110615bf72cb38d551012a7100a
Md Tarikul Islam <tarikulnayan1998@gmail.com>
2025-12-05 15:53:51 +0600

    added files

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,26 @@
-const router = () => {
-    return (
-        <div>
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
 
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout></HomeLayout>,
+    },
+    {
+        path: "/news",
+        element: <h1>News Layout</h1>,
+    },
+    {
+        path: "auth",
+        element: <h1>Login</h1>,
+    },
+    {
+        path: "*",
+        element: <h1>Error</h1>,
+    },
+]);
 
-export default router;
+export default router
