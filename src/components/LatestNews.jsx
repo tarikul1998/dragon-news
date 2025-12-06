acb100bdb3ee279510103ea1bb8d1b0d809fda58
Md Tarikul Islam <tarikulnayan1998@gmail.com>
2025-12-06 22:29:09 +0600

    added files

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -6,18 +6,21 @@ const LatestNews = () => {
         <div className="flex gap-2 bg-base-200-200 p-2">
             <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
             <Marquee className="space-x-10" pauseOnHover={true} speed={100}>
-                <Link to='/news'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, corrupti.
+                <Link to="/news">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem,
+                    corrupti.
                 </Link>
-                <Link to='/news'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, corrupti.
+                <Link to="/news">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem,
+                    corrupti.
                 </Link>
-                <Link to='/news'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, corrupti.
+                <Link to="/news">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem,
+                    corrupti.
                 </Link>
             </Marquee>
         </div>
     );
 };
 
-export default LatestNews; 
+export default LatestNews;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
     return (
@@ -10,6 +11,14 @@ const HomeLayout = () => {
                     <LatestNews></LatestNews>
                 </section>
             </header>
+            <nav className="w-11/12 max-auto py-2">
+                <Navbar></Navbar>
+            </nav>
+            <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+                <aside className="col-span-3">Left Navbar</aside>
+                <section className="col-span-6">Main Content</section>
+                <aside className="right col-span-3">Right Navbar</aside>
+            </main>
         </div>
     );
 };
